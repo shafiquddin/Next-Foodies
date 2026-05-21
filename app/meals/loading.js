d7713a7b8d904a92f590381370d9ecc8a10fc5c3
shafiquddin <shafiquddin2k@gmail.com>
2026-05-21 18:06:43 +0530

    sqllite backend add

---
+++ app/meals/loading.js
@@ -0,0 +1,8 @@
+import React from "react";
+import classes from "./loading.module.css";
+
+const MealsLoadingPage = () => {
+  return <p className={classes.loading}>Fetching Meals...</p>;
+};
+
+export default MealsLoadingPage;
