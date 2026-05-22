5a8899afcb022c46643a9f47e1f722388b328798
shafiquddin <shafiquddin2k@gmail.com>
2026-05-22 12:37:45 +0530

    Error handling and Loading page and using suspense loading

---
+++ app/meals/error.js
@@ -0,0 +1,14 @@
+"use client";
+
+import React from "react";
+
+const Error = () => {
+  return (
+    <div className="error">
+      <h1>An Error Occured!</h1>
+      <p>Failed to fetch meal data, Please try again later!</p>
+    </div>
+  );
+};
+
+export default Error;
--- app/meals/page.js
+++ app/meals/page.js
@@ -2,9 +2,14 @@ import Link from "next/link";
 import classes from "./page.module.css";
 import MealsGrid from "@/components/meals/meals-grid";
 import { getMeals } from "@/lib/meals";
+import { Suspense } from "react";
+
+const FetchMeals = async () => {
+  const meals = await getMeals();
+  return <MealsGrid meals={meals} />;
+};
 
 export default async function Meals() {
-  const meals = await getMeals();
   return (
     <>
       <header className={classes.header}>
@@ -20,7 +25,11 @@ export default async function Meals() {
         </p>
       </header>
       <main className={classes.main}>
-        <MealsGrid meals={meals} />
+        <Suspense
+          fallback={<p className={classes.loading}>Fetching Meals...</p>}
+        >
+          <FetchMeals />
+        </Suspense>
       </main>
     </>
   );
