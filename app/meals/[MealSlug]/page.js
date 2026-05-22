c608ef4b02e394fbed7903873cbc0699d27dcef5
shafiquddin <shafiquddin2k@gmail.com>
2026-05-22 14:54:14 +0530

    meals details

--- app/meals/[MealSlug]/page.js
+++ app/meals/[MealSlug]/page.js
@@ -1,9 +1,34 @@
+import Image from "next/image";
+import classes from "./page.module.css";
+import { getMeal } from "@/lib/meals";
+
 const MealsDetails = async ({ params }) => {
-  const { slug } = await params;
+  const { mealSlug } = await params;
+  const meal = await getMeal(mealSlug);
+  meal.instructions = meal.instructions.replace(/\n/g, "<br />");
+
   return (
     <>
-      <h1 style={{ color: "white", textAlign: "center" }}>Meals Details</h1>
-      <p>{slug}</p>
+      <header className={classes.header}>
+        <div className={classes.image}>
+          <Image src={meal.image} alt={meal.title} fill />
+        </div>
+        <div className={classes.headerText}>
+          <h1>{meal.title}</h1>
+          <p className={classes.creator}>
+            by <a href={`mailto:${meal.creator_email}`}>{meal.creator}</a>
+          </p>
+          <p className={classes.summary}>{meal.summary}</p>
+        </div>
+      </header>
+      <main className={classes.main}>
+        <p
+          className={classes.instructions}
+          dangerouslySetInnerHTML={{
+            __html: meal.instructions,
+          }}
+        ></p>
+      </main>
     </>
   );
 };
---
+++ app/not-found.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const NotFound = () => {
+  return (
+    <main className="not-found">
+      <h1>Not Found</h1>
+      <p>Unfortunately, We could not find the requested page or response</p>
+    </main>
+  );
+};
+
+export default NotFound;
